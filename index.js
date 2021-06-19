5dc6171468aaf4c0ad8ffe68b2024fff0a9b3566
shanghua521 <2621197736@qq.com>
2021-06-19 18:29:25 +0800

    fix bug

--- index.js
+++ index.js
@@ -1,16 +1,21 @@
 var assign = require('deep-assign');
 var renderer = require('./lib/renderer');
 
-hexo.config.flowchart = assign({
-  webfont: 'https://cdnjs.cloudflare.com/ajax/libs/webfont/1.6.27/webfontloader.js',
-  snap: 'https://cdnjs.cloudflare.com/ajax/libs/snap.svg/0.4.1/snap.svg-min.js',
-  underscore: 'https://cdnjs.cloudflare.com/ajax/libs/underscore.js/1.8.3/underscore-min.js',
-  sequence: 'https://cdnjs.cloudflare.com/ajax/libs/js-sequence-diagrams/1.0.6/sequence-diagram-min.js',
-  css: '',
-  options: {
-    theme: 'simple'
-  }
-}, hexo.config.flowchart);
+hexo.config.sequence = assign(
+  {
+    raphael: "https://cdn.bootcss.com/raphael/2.2.8/raphael.min.js",
+    webfont: "https://cdn.bootcss.com/webfont/1.6.28/webfontloader.js",
+    snap: "https://cdn.bootcss.com/snap.svg/0.5.1/snap.svg-min.js",
+    underscore: "https://cdn.bootcss.com/underscore.js/1.9.1/underscore-min.js",
+    sequence:
+      "https://cdn.bootcss.com/js-sequence-diagrams/1.0.6/sequence-diagram-min.js",
+    css: "",
+    options: {
+      theme: "simple",
+    },
+  },
+  hexo.config.sequence
+);
 
 hexo.extend.filter.register('before_post_render', renderer.render, 9);
 
